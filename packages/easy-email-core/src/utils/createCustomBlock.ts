7a17b25353de400689f67c17a6093ce702603f13
ch.mao <ch.mao@MacBook-Pro.local>
2022-01-12 16:38:04 +0800

    :bug: fix typings

--- packages/easy-email-core/src/utils/createCustomBlock.ts
+++ packages/easy-email-core/src/utils/createCustomBlock.ts
@@ -3,18 +3,17 @@ import { IBlock, IBlockData } from '@core/typings';
 import { ReactElement } from 'react';
 
 interface CreateBlockOption<T extends IBlockData>
-  extends Omit<IBlock<T>, 'transform'> { }
+  extends Omit<IBlock<T>, 'transform'> {}
 
 export function createCustomBlock<T extends IBlockData>(
   block: CreateBlockOption<T> & {
-
     render: (
       data: T,
       idx: string | null,
       mode: 'testing' | 'production',
       context?: IPage,
-      dataSource?: { [key: string]: any; }
-    ) => IBlockData | ReactElement;
+      dataSource?: { [key: string]: any }
+    ) => IBlockData | ReactElement | null;
   }
 ): IBlock<T> {
   return {
