98c3789557df78dee7fe1e82cb774eac51eb63b6
ch.mao <ch.mao@MacBook-Pro.local>
2022-01-13 11:44:36 +0800

    :sparkles: update "Hero" block style

--- packages/easy-email-core/src/blocks/Hero/index.tsx
+++ packages/easy-email-core/src/blocks/Hero/index.tsx
@@ -1,8 +1,6 @@
 import { IBlockData } from '@core/typings';
 import { BasicType } from '@core/constants';
 import { merge } from 'lodash';
-import { Button } from '../Button';
-import { Text } from '../Text';
 import { createBlock } from '@core/utils/createBlock';
 export type IHero = IBlockData<
   {
@@ -37,32 +35,76 @@ export const Hero = createBlock<IHero>({
         padding: '100px 0px 100px 0px',
         'vertical-align': 'top',
         'background-url':
-          'https://cloud.githubusercontent.com/assets/1830348/15354890/1442159a-1cf0-11e6-92b1-b861dadf1750.jpg',
+          'https://easy-email-m-ryan.vercel.app/images/dd1584fb-cb60-42c9-80c7-5545e16130ca-226ba72b-ce9e-4948-ad0d-347381fb96c5.png',
       },
       children: [
-        Text.create({
+        {
+          type: 'text',
           data: {
             value: {
-              content: 'GO TO SPACE',
+              content: 'We Serve Healthy &amp; Delicious Foods',
             },
           },
           attributes: {
-            color: '#ffffff',
+            padding: '10px 25px 10px 25px',
             align: 'center',
+            color: '#ffffff',
             'font-size': '45px',
             'line-height': '45px',
           },
-        }),
-        Button.create({
+          children: [],
+        },
+        {
+          type: 'text',
           data: {
             value: {
-              content: 'ORDER YOUR TICKET NOW',
+              content:
+                'A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.<br>',
             },
           },
           attributes: {
+            align: 'center',
+            'background-color': '#414141',
+            color: '#ffffff',
+            'font-weight': 'normal',
+            'border-radius': '3px',
+            padding: '10px 25px 10px 25px',
+            'inner-padding': '10px 25px 10px 25px',
+            'line-height': '1.5',
+            target: '_blank',
+            'vertical-align': 'middle',
+            border: 'none',
+            'text-align': 'center',
+            href: '#',
+            'font-size': '14px',
+          },
+          children: [],
+        },
+        {
+          type: 'button',
+          data: {
+            value: {
+              content: 'Get Your Order Here!',
+            },
+          },
+          attributes: {
+            align: 'center',
+            'background-color': '#f3a333',
+            color: '#ffffff',
+            'font-size': '13px',
+            'font-weight': 'normal',
+            'border-radius': '30px',
+            padding: '10px 25px 10px 25px',
+            'inner-padding': '10px 25px 10px 25px',
+            'line-height': '120%',
+            target: '_blank',
+            'vertical-align': 'middle',
+            border: 'none',
+            'text-align': 'center',
             href: '#',
           },
-        }),
+          children: [],
+        },
       ],
     };
     return merge(defaultData, payload);
