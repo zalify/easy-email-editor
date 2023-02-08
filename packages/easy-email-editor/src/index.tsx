efedff4ba79806dd000cf3c14bb3149ba3fb23e6
William Lauzé <william.lauze@gmail.com>
2023-02-08 13:44:25 -0500

    Update packages/easy-email-editor/src/index.tsx

--- packages/easy-email-editor/src/index.tsx
+++ packages/easy-email-editor/src/index.tsx
@@ -6,9 +6,9 @@ export { BlockAvatarWrapper } from './components/wrapper';
 export { EmailEditor } from './components/EmailEditor';
 
 // exposing more granular components
-export { EditEmailPreview } from'./components/EmailEditor/components/EditEmailPreview'
-export { MobileEmailPreview } from'./components/EmailEditor/components/MobileEmailPreview'
-export { DesktopEmailPreview } from'./components/EmailEditor/components/DesktopEmailPreview'
+export { EditEmailPreview } from'./components/EmailEditor/components/EditEmailPreview';
+export { MobileEmailPreview } from'./components/EmailEditor/components/MobileEmailPreview';
+export { DesktopEmailPreview } from'./components/EmailEditor/components/DesktopEmailPreview';
 
 // export utils
 export * from './utils/index';
