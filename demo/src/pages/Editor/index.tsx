4e6b01d50ee7ed4975fdd46b63c53c8b09eb7c87
Ryan <962491243@qq.com>
2023-07-12 23:13:07 +0800

    Update index.tsx

--- demo/src/pages/Editor/index.tsx
+++ demo/src/pages/Editor/index.tsx
@@ -633,8 +633,8 @@ export default function Editor() {
                           <strong>Export</strong>
                         </Button>
                       </Dropdown>
-                      <Button onClick={() => setVisible(true)}>
-                        <strong>Try responsive editor</strong>
+                      <Button type="text" href="mailto:962491243@qq.com">
+                        <strong>Try advanced editor</strong>
                       </Button>
                       <a
                         href='https://www.buymeacoffee.com/easyemail?utm_source=webside&utm_medium=button&utm_content=donate'
