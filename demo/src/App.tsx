9ae9a2d7ba1f017e5fd14c08f42148f8408c353e
cnh1en <cnhien12@gmail.com>
2023-12-28 22:36:48 +0700

    feat: add polaris

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -6,56 +6,61 @@ import store from '@demo/store';
 import '@demo/styles/common.scss';
 import { history } from './utils/history';
 import Home from '@demo/pages/Home';
+import enTranslations from '@shopify/polaris/locales/en.json';
+import { AppProvider, Button } from '@shopify/polaris';
+import '@shopify/polaris/build/esm/styles.css';
 
 const Editor = React.lazy(() => import('@demo/pages/Editor'));
 
 function App() {
   return (
-    <Provider store={store}>
-      <Page>
-        <Suspense
-          fallback={
-            <div
-              style={{
-                width: '100vw',
-                height: '100vh',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}
-            >
-              <img
-                width='200px'
-                src='/loading'
-                alt=''
-              />
-              <p
+    <AppProvider i18n={enTranslations}>
+      <Provider store={store}>
+        <Page>
+          <Suspense
+            fallback={
+              <div
                 style={{
-                  fontSize: 24,
-                  color: 'rgba(0, 0, 0, 0.65)',
+                  width: '100vw',
+                  height: '100vh',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
                 }}
               >
-                Please wait a moment.
-              </p>
-            </div>
-          }
-        >
-          <Router history={history}>
-            <Switch>
-              <Route
-                path='/'
-                exact
-                component={Home}
-              />
-              <Route
-                path='/editor'
-                component={Editor}
-              />
-            </Switch>
-          </Router>
-        </Suspense>
-      </Page>
-    </Provider>
+                <img
+                  width='200px'
+                  src='/loading'
+                  alt=''
+                />
+                <p
+                  style={{
+                    fontSize: 24,
+                    color: 'rgba(0, 0, 0, 0.65)',
+                  }}
+                >
+                  Please wait a moment.
+                </p>
+              </div>
+            }
+          >
+            <Router history={history}>
+              <Switch>
+                <Route
+                  path='/'
+                  exact
+                  component={Home}
+                />
+                <Route
+                  path='/editor'
+                  component={Editor}
+                />
+              </Switch>
+            </Router>
+          </Suspense>
+        </Page>
+      </Provider>
+    </AppProvider>
   );
 }
 
